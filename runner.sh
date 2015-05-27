c7ffe0e4c09787da13e43d21ee83d3dbacc80056
CaptainMac <support@serveur-web.org>
2015-05-27 13:48:51 +0200

    Revert "Revert "commit""
    This reverts commit 2cdfe85dbdeba666291dd906b8fbe4f3614055ee.

---
+++ runner.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+[ -d /dev/net ] ||
+    mkdir -p /dev/net
+[ -c /dev/net/tun ] ||
+    mknod /dev/net/tun c 10 200
+
+if [ ! -d "/var/log/vpnserver/security_log" ]; then
+  mkdir -p /var/log/vpnserver/security_log
+fi
+
+if [ ! -d "/var/log/vpnserver/packet_log" ]; then
+  mkdir -p /var/log/vpnserver/packet_log
+fi
+
+if [ ! -d "/var/log/vpnserver/server_log" ]; then
+  mkdir -p /var/log/vpnserver/server_log
+fi
+
+ln -s /var/log/vpnserver/*_log /usr/local/vpnserver/
+
+exec /usr/local/vpnserver/vpnserver execsvc
+
+exit $?
